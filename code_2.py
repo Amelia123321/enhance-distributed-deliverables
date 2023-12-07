eeb9b00b244bc6d296339aae9e247e16383de276
Alersandr <151876621+Amelia123321@users.noreply.github.com>
2023-12-07 05:13:43 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,42 @@
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    main()
+
+import random
+    for item in data:
+    return data
+def generate_random_data():
+    data = generate_random_data()
+
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]    return data;
+function main() {
+    data.forEach(item => {
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    for (let i = 0; i < 10; i++) {
+main();
+    const data = [];
+
+    });
+}
+
+}    return data
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def generate_random_data():
+    main()
+    for item in data:
